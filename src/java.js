3f293727d257721180c50d1edb719cebdeb2ac4e
IrinaWilson <keysii@yandex.ru>
2023-04-01 15:52:20 +1100

    added date in JS

--- src/java.js
+++ src/java.js
@@ -1,1 +1,40 @@
+let now = new Date();
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednsday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let date = now.getDate();
+let month = months[now.getMonth()];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0 ${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let sDate = document.querySelector(".date");
+sDate.innerHTML = `${day}, ${date} ${month}`;
+let sTime = document.querySelector(".time");
+sTime.innerHTML = `${hours}:${minutes}`;
