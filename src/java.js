6e939db100f061a2bb062035217707066d5574cf
IrinaWilson <keysii@yandex.ru>
2023-04-03 10:27:04 +1000

    added JS for "current" btn

--- src/java.js
+++ src/java.js
@@ -71,3 +71,16 @@ function showTemperature(response) {
 
   span2.innerHTML = `${cityName}`;
 }
+
+function showPosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("#getPosition");
+button.addEventListener("click", getCurrentPosition);
